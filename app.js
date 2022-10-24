48d4003e63384e0e45e1180bc525ccae573deab6
SIta Dabo <euphoria214rue@gmail.com>
2022-10-25 00:48:09 +0100

    installed dependencies etc
    added basic routes, public folder

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-// requiring of packages
+// requiring packgaes
 
-const express = require('express');
-const nodemailer = require('nodemailer');
+const express= require('express');
+const nm = require("nodemailer");
+const bodyParser = require('body-parser');
+
+const port = 3000;
+const app = express();
+
+app.listen(port, ()=> console.log('Server Started'));
+app.set('view engine', 'ejs');
+app.set
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// setting Express static folder
+express.static('public')
+
+
+app.get('/', (req, res)=>{
+        res.send('Hello World')
+})

851bd69179aa00278679c68bf4bee46e2dab7b68
SIta Dabo <euphoria214rue@gmail.com>
2022-10-25 01:02:02 +0100

    mail transporter function
    this is to help send the email using node and nodemailer

--- app.js
+++ app.js
@@ -23,3 +23,43 @@ express.static('public')
 app.get('/', (req, res)=>{
         res.send('Hello World')
 })
+
+app.post('/send', (req, res)=>{
+    const output = `
+    <p>you have a new contact reques</p>
+    
+    <ul>
+    <li>Name : ${req.body.name}</li>
+    <li>Name : ${req.body.company}</li>
+    <li>Name : ${req.body.email}</li>
+    <li>Name : ${req.body.phone}</li></ul>
+    `  
+
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass, // generated ethereal password
+        },
+      });
+    
+      // send mail with defined transport object
+      let info =  transporter.sendMail({
+        from: '"Fred Foo 👻" <sender email>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    
+})

cbba6bbdc3e0cd310448c283cd03d61844920278
SIta Dabo <euphoria214rue@gmail.com>
2022-10-29 02:09:49 +0100

    landing page build
    this is the build of the landing page basics

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ const app = express();
 
 app.listen(port, ()=> console.log('Server Started'));
 app.set('view engine', 'ejs');
-app.set
+app.set('views', 'views');
+
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-// setting Express static folder
-express.static('public')
+// Use express Static
+app.use(express.static('public'))
+
 
 
 app.get('/', (req, res)=>{
-        res.send('Hello World')
+        res.render('index', { title : 'Home'})
 })
 
 app.post('/send', (req, res)=>{

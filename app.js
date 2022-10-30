59e278bb299eff62775b9c6da1e41f165ccc5bf6
SIta Dabo <euphoria214rue@gmail.com>
2022-10-30 19:07:11 +0100

    updates
    updates

--- app.js
+++ app.js
@@ -28,15 +28,9 @@ app.get('/', (req, res)=>{
 
 app.post('/send', (req, res)=>{
     const output = `
-    <p>you have a new contact reques</p>
     
-    <ul>
-    <li>Name : ${req.body.name}</li>
-    <li>Name : ${req.body.company}</li>
-    <li>Name : ${req.body.email}</li>
-    <li>Name : ${req.body.phone}</li></ul>
     `  
-
+    // ${req.body.company}
 
     let transporter = nodemailer.createTransport({
         host: "smtp.ethereal.email",
@@ -53,18 +47,16 @@ app.post('/send', (req, res)=>{
     
       // send mail with defined transport object
       let info =  transporter.sendMail({
-        from: '"Fred Foo 👻" <sender email>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
+        from: '"Mail Depot" <sender email>', // sender address
+        to: useremail, // list of receivers
+        subject: subject, // Sububject line
+        text: body, // plain text body
         html: "<b>Hello world?</b>", // html body
       });
     
       console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    
-      // Preview only available when sending through an Ethereal account
+  
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
     
 })

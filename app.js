e1e69ec8cc1c4d790f60ffb0fb989555c9fac02f
SIta Dabo <euphoria214rue@gmail.com>
2022-11-21 12:40:56 +0100

    obad

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
@@ -36,36 +36,7 @@ app.get('/send', (req, res)=>{
 })
 
 app.post('/send', (req, res)=>{
-    const output = `
-    
-    `  
-    // ${req.body.company}
+   console.log(req.body)
 
-    let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: testAccount.user, // generated ethereal user
-          pass: testAccount.pass, // generated ethereal password
-        },
-        tls:{
-            rejectUnauthorized:false
-        }
-      });
-    
-      // send mail with defined transport object
-      let info =  transporter.sendMail({
-        from: '"Mail Depot" <sender email>', // sender address
-        to: useremail, // list of receivers
-        subject: subject, // Sububject line
-        text: body, // plain text body
-        html: "<b>Hello world?</b>", // html body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
-  
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-    
+    res.send(req.body)
 })
